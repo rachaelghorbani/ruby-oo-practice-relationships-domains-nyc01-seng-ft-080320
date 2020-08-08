230dd7723150783a0275fdb93f45b26fb5a08147
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-08-08 13:49:01 -0400

    completed airbnb lab

--- app/models/airbnb/listing.rb
+++ app/models/airbnb/listing.rb
@@ -1,52 +1,47 @@
+class Listing
+  attr_accessor :city
 
-  
+  @@all = []
 
-# You are building an app for an Airbnb competitor
+  def initialize(city)
+    @city = city
+    @@all << self
+  end
 
-# - your models are listings, guests, trips
-#   - a listing (i.e. a house or apartment) has many trips
-#     - a listing has a city attribute which is a string of a city name, e.g. 'Seattle'
-#   - a guest has many trips
-#   - a trip belongs to a listing and a guest
+  def self.all
+    @@all
+  end
 
-# Write out the relationships using has_many, belongs_to and has_many_through.
-# Create the necessary methods to connect these classes.
+  def self.find_all_by_city(city)
+    Listing.all.select do |listing|
+      listing.city == city
+    end
+  end
 
-# #### Listing
+  def self.most_popular
+    counts = Listing.all.map do |listing|
+        listing.trip_count
+    end
+    Listing.all.find do |listing|
+        listing.trip_count == counts.max
+    end
+  end
 
-# - #guests
-#   - returns an array of all guests who have stayed at a listing
-# - #trips
-#   - returns an array of all trips at a listing
-# - #trip_count
-#   - returns the number of trips that have been taken to that listing
-# - .all
-#   - returns an array of all listings
-# - .find_all_by_city(city)
-#   - takes an argument of a city name (as a string) and returns all the listings for that city
-# - .most_popular
-#   - finds the listing that has had the most trips
+  def trips
+    Trip.all.select do |trip|
+      trip.listing == self
+    end
+  end
 
-# #### Guest
+  def guests
+    self.trips.map do |trip|
+      trip.guest
+    end
+  end
 
-# - #listings
-#   - returns an array of all listings a guest has stayed at
-# - #trips
-#   - returns an array of all trips a guest has made
-# - #trip_count
-#   - returns the number of trips a guest has taken
-# - .all
-#   - returns an array of all guests
-# - .pro_traveller
-#   - returns an array of all guests who have made over 1 trip
-# - .find_all_by_name(name)
-#   - takes an argument of a name (as a string), returns the all guests with that name
+  def trip_count
+    self.trips.count
+  end
 
-# #### Trip
+end
 
-# - #listing
-#   - returns the listing object for the trip
-# - #guest
-#   - returns the guest object for the trip
-# - .all
-#   - returns an array of all trips
--- tools/console.rb
+++ tools/console.rb
@@ -4,7 +4,30 @@ def reload
   load 'config/environment.rb'
 end
 
+######## GUESTS ########
 
+guest1 = Guest.new("Rachael")
+guest2 = Guest.new("Valdet")
+guest3 = Guest.new("Natacha")
+guest4 = Guest.new("Blade")
+guest5 = Guest.new("Blade")
+
+######## LISTINGS ########
+
+listing1 = Listing.new("Madrid")
+listing2 = Listing.new("Paris")
+listing3 = Listing.new("Sydney")
+listing4 = Listing.new("Tehran")
+listing5 = Listing.new("Tehran")
+
+######## TRIPS ########
+
+trip1 = Trip.new(guest1, listing1)
+trip2 = Trip.new(guest2, listing2)
+trip3 = Trip.new(guest3, listing1)
+trip4 = Trip.new(guest4, listing1)
+trip5 = Trip.new(guest2, listing3)
+trip5 = Trip.new(guest4, listing1)
 
 
 Pry.start
